fdb40bef7ccb822825464cd763761ca451788ce4
EGC-pabcubcru <pabcubcru@alum.us.es>
2021-02-12 10:22:11 +0000

    Modificado settings.py para heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -73,7 +73,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://pabcubcruexamen1230.herokuapp.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -168,6 +168,8 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+APIS = {}
+
 try:
     from local_settings import *
 except ImportError:
@@ -183,3 +185,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
